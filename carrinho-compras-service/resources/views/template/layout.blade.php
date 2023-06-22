22bc444740b82c586a2f72fde8877b7b534a470f
castagnagh <castagnagh>
2023-06-22 01:26:25 -0300

    front index finalizado

--- carrinho-compras-service/resources/views/template/layout.blade.php
+++ carrinho-compras-service/resources/views/template/layout.blade.php
@@ -7,8 +7,35 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../../js/app.js">
-    <link rel="stylesheet" type="text/css" href="../../css/app.css">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script> --}}
+    <style>
+    .carrinho-img {
+        width: 150px;
+    }
+    
+    .card-produtos {
+        height: 400px;
+    }
+    .card-img-top {
+        height: 200px;
+        object-fit: cover;
+    }
+    .card-hover {
+    transition: transform 0.3s, box-shadow 0.3s;
+    }
+
+    .card-hover:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .card-wide {
+    width: 100%; /* Ajuste o valor conforme necessário */
+    margin: 0 auto; /* Centralizar os cards horizontalmente */
+}
+
+    </style>
     <title>ByteStore</title>
 </head>
 <body>
